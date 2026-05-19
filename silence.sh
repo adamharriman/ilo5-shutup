59334644c0f08803ca7036806a0632b0fa96164e
adamharriman <96307878+adamharriman@users.noreply.github.com>
2026-05-19 14:48:13 +0200

    ilo_silence_token.sh
    Ilo5 dows not accept commands without an open session. The session token is need to send any commands.
    The updated script will first find the token then send the adjustment using it.

--- silence.sh
+++ silence.sh
@@ -1,1 +1,18 @@
-curl --request PATCH --url 'https://192.168.178.113/redfish/v1/Chassis/1/Thermal/' --user 'USERNAME:PASSWORD' --header 'content-type: application/json' --insecure --data '{"Oem": {"Hpe": {"FanPercentAdjust": 0}}}'
+#!/bin/bash
+
+ILO_HOST="127.0.0.1"
+ILO_USER="Administrator"
+ILO_PASS="Password"
+FAN_ADJUST=0
+
+TOKEN=$(curl -sk -X POST "https://${ILO_HOST}/redfish/v1/SessionService/Sessions/" \
+  -H 'content-type: application/json' \
+  -d "{\"UserName\":\"${ILO_USER}\",\"Password\":\"${ILO_PASS}\"}" \
+  -D - | grep -i x-auth-token | awk '{print $2}' | tr -d '\r')
+
+[ -z "$TOKEN" ] && exit 1
+
+curl -sk -X PATCH "https://${ILO_HOST}/redfish/v1/Chassis/1/Thermal/" \
+  -H 'content-type: application/json' \
+  -H "X-Auth-Token: $TOKEN" \
+  --data "{\"Oem\": {\"Hpe\": {\"FanPercentAdjust\": ${FAN_ADJUST}}}}"
